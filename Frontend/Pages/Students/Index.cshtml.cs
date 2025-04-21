3beae51c87a26454c799f2730c48352906901a71
ndcuong2k3 <21a100100051@students.hou.edu.vn>
2025-04-21 09:16:03 +0700

    Update Project

--- Frontend/Pages/Students/Index.cshtml.cs
+++ Frontend/Pages/Students/Index.cshtml.cs
@@ -1,3 +1,4 @@
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using StudentService.Models;
 
@@ -6,7 +7,6 @@ namespace Frontend.Pages.Students;
 public class IndexModel : PageModel
 {
     private readonly HttpClient _httpClient;
-
     public List<Student>? Students { get; set; }
 
     public IndexModel(IHttpClientFactory httpClientFactory)
@@ -18,4 +18,21 @@ public class IndexModel : PageModel
     {
         Students = await _httpClient.GetFromJsonAsync<List<Student>>("/api/students/GetStudents");
     }
+
+    public async Task<IActionResult> OnPostDeleteAsync(int id)
+    {
+        var response = await _httpClient.DeleteAsync($"/api/students/DeleteStudent/{id}");
+
+        if (response.IsSuccessStatusCode)
+        {
+            return RedirectToPage();
+        }
+        else
+        {
+            ModelState.AddModelError(string.Empty, "Xóa học sinh thất bại.");
+            await OnGetAsync(); 
+            return Page();
+        }
+    }
+
 }
